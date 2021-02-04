df1dfedca990b27a394075d2ec8fe722133f70bd
hanwenlin <1353516732@qq.com>
2021-02-04 22:48:57 +0800

    commit 00

---
+++ tensorflow2-keras-tur/lesson2-keras-func-api/network-simple.py
@@ -0,0 +1,84 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+
+# create network
+inputs = keras.Input(shape=(784,), name='img')
+h1 = layers.Dense(32, activation='relu')(inputs)
+h2 = layers.Dense(32, activation='relu')(h1)
+outputs = layers.Dense(10, activation='softmax')(h2)
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.summary()
+keras.utils.plot_model(model, 'mnist_model.png')
+keras.utils.plot_model(model, 'model_info.png', show_shapes=True)
+
+# train validation and test
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(60000, 784).astype('float32')/255.
+x_test = x_test.reshape(10000, 784).astype('float32')/255.
+
+model.compile(optimizer=keras.optimizers.RMSprop(),
+              loss='sparse_categorical_crossentropy',   #
+              metrics=['accuracy'])
+history = model.fit(x_train, y_train, batch_size=64, epochs=5, validation_split=0.1, verbose=1)
+test_scores = model.evaluate(x_test, y_test, verbose=1)
+print('test loss: ', test_scores[0])
+print('test acc: ', test_scores[1])
+
+# model_sustain and serialization
+model.save('model_save.h5')
+del model
+model = keras.models.load_model('model_save.h5')
+
+# creating multi-model by sharing the networking
+# 在函数API中，通过在图层图中指定其输入和输出来创建模型。 这意味着可以使用单个图层图来生成多个模型。
+encode_input = keras.Input(shape=(28, 28, 1), name='img')
+h1 = layers.Conv2D(16, 3, activation='relu')(encode_input)
+h1 = layers.Conv2D(32, 3, activation='relu')(h1)
+h1 = layers.MaxPool2D(2)(h1)
+h1 = layers.Conv2D(32, 3, activation='relu')(h1)
+h1 = layers.Conv2D(16, 3, activation='relu')(h1)
+encode_output = layers.GlobalMaxPool2D()(h1)
+
+encode_model = keras.Model(inputs=encode_input, outputs=encode_output, name='encoder')
+encode_model.summary()
+
+h2 = layers.Reshape((4, 4, 1))(encode_output)
+h2 = layers.Conv2DTranspose(16, 3, activation='relu')(h2)
+h2 = layers.Conv2DTranspose(32, 3, activation='relu')(h2)
+h2 = layers.UpSampling2D(3)(h2)
+h2 = layers.Conv2DTranspose(16, 3, activation='relu')(h2)
+decode_output = layers.Conv2DTranspose(1, 3, activation='relu')(h2)
+autoencoder = keras.Model(inputs=encode_input, outputs=decode_output, name='autoencoder')
+autoencoder.summary()
+
+# 可以把整个模型，当作一层网络使用
+encode_input = keras.Input(shape=(28,28,1), name='src_img')
+h1 = layers.Conv2D(16, 3, activation='relu')(encode_input)
+h1 = layers.Conv2D(32, 3, activation='relu')(h1)
+h1 = layers.MaxPool2D(3)(h1)
+h1 = layers.Conv2D(32, 3, activation='relu')(h1)
+h1 = layers.Conv2D(16, 3, activation='relu')(h1)
+encode_output = layers.GlobalMaxPool2D()(h1)
+
+encode_model = keras.Model(inputs=encode_input, outputs=encode_output, name='encoder2')
+encode_model.summary()
+
+decode_input = keras.Input(shape=(16, ), name='encoded_img')
+h2 = layers.Reshape((4, 4, 1))(decode_input)
+h2 = layers.Conv2DTranspose(16, 3, activation='relu')(h2)
+h2 = layers.Conv2DTranspose(32, 3, activation='relu')(h2)
+h2 = layers.UpSampling2D(3)(h2)
+h2 = layers.Conv2DTranspose(16, 3, activation='relu')(h2)
+decode_output = layers.Conv2DTranspose(1, 3, activation='relu')(h2)
+decode_model = keras.Model(inputs=decode_input, outputs=decode_output, name='decoders2')
+decode_model.summary()
+
+autoencoder_input = keras.Input(shape=(28,28,1), name='img')
+h3 = encode_model(autoencoder_input)
+autoencoder_output = decode_model(h3)
+autoencoder = keras.Model(inputs=autoencoder_input, outputs=autoencoder_output, name='autoencoder')
+autoencoder.summary()
